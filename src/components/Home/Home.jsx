5c2d997cde2ddb67afec4e49c4907abc89ad1563
pugalendhi221 <pukalpukal720@gmail.com>
2024-06-11 13:21:21 +0530

    Functionalities download cv name animation

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,26 +1,76 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Home.css';
+import pdfFile from '../../assets/Resume/Resume.pdf';
 
 const Home = () => {
+    const [text, setText] = useState('');
+    const [typingIndex, setTypingIndex] = useState(0);
+    const [isReversed, setIsReversed] = useState(false);
+
+    const introText = "Hi I'm Pugalendhi";
+
+    const handleDownloadCv = () => {
+        // Fetch the PDF file asynchronously
+        fetch(pdfFile)
+            .then(response => response.blob())
+            .then(blob => {
+                // Create a temporary URL for the Blob object
+                const url = URL.createObjectURL(blob);
+    
+                // Open the PDF file in a new browser tab/window
+                window.open(url, '_blank');
+    
+                // Release the temporary URL
+                URL.revokeObjectURL(url);
+            })
+            .catch(error => {
+                console.error('Error fetching PDF file:', error);
+            });
+    };
+    
+
+    useEffect(() => {
+        const typingTimer = setTimeout(() => {
+            if (!isReversed) {
+                if (typingIndex <= introText.length) {
+                    setText(introText.slice(0, typingIndex));
+                    setTypingIndex(typingIndex + 1);
+                } else {
+                    setIsReversed(true);
+                }
+            } else {
+                if (typingIndex >= 0) {
+                    setText(introText.slice(0, typingIndex));
+                    setTypingIndex(typingIndex - 1);
+                } else {
+                    setIsReversed(false);
+                    setTypingIndex(0);
+                }
+            }
+        }, 200);
+
+        return () => clearTimeout(typingTimer);
+    }, [typingIndex, isReversed]);
+
     return (
         <div id="Home" className='home-container'>
-            <div className='left-container'>
-                <div id='name'>
-                    <p>WELCOME TO MY WORLD</p>
-                    <h1>Hi I'm Pugalendhi</h1>
-                    <h4>Software developer</h4>
-                    <p>Lorem Ipsum is a placeholder text used in publishing and graphic design to demonstrate the look and feel of a layout before the actual content is available. It is a scrambled version of a Latin text, which makes it look like real text but has no meaning.</p>
-                </div>
-                <div className='button-container'>
-                    <button>Download CV</button>
-                    <button id='contact'>Contact Me</button>
-                </div>
-            </div>
             <div className='right-container'>
                 <div id='profile-container'>
                     <img src='src/assets/afro-man-saluting.jpg' alt='profile pic' />
                 </div>
-                <div></div>
+            </div>
+            <div className='left-container'>
+                <div id='name'>
+                    <p>WELCOME TO MY WORLD</p>
+                    <h1 className="typing-text">{text}</h1>
+                </div>
+                <div>
+                    <h4>Software developer</h4>
+                    <p>Hello! My name is Pukalendhi, and I am a Mobile Application Developer with 1 year of experience. I specialize in creating seamless and engaging mobile apps. Additionally, I have skills in building websites using React.js for the frontend. Currently, I am expanding my knowledge by learning backend technologies. I am passionate about technology and constantly strive to enhance my skills and deliver exceptional digital experiences.</p>
+                </div>
+                <div className='button-container'>
+                    <button onClick={handleDownloadCv}>Download CV</button>
+                </div>
             </div>
         </div>
     );
